34d28717781322d16b199b0c04952822c264473d
Antti H <anaheino@gmail.com>
2023-09-22 12:03:14 +0200

    Removed unnecessary stuff from vue app

--- ufo_backend/main.go
+++ ufo_backend/main.go
@@ -13,27 +13,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"ufo_backend/structs"
 )
 
-type Sighting struct {
-	ID string `json:"_id"`
-	Date string `json:"date"`
-	City string `json:"city"`
-	State string `json:"state"`
-	Country string `json:"country"`
-	Shape string `json:"shape"`
-	Duration string `json:"duration"`
-	Description string `json:"description"`
-	ReportDate string `json:"report_date"`
-	HasImages bool `json:"has_images"`
-	Link string `json:"link"`
-	Latitude float64 `json:"latitude"`
-	Longitude float64 `json:"longitude"`
-}
 var sightingsCollection *mongo.Collection
 
 func getSightings(c *gin.Context) {
-	var results []Sighting
+	var results []structs.Sighting
 	cursor, err := sightingsCollection.Find(context.TODO(), bson.D{{"country", "Finland"}})
 	if err != nil {
 		fmt.Printf(err.Error())
---
+++ ufo_backend/structs/sighting.go
@@ -0,0 +1,17 @@
+package structs
+
+type Sighting struct {
+	ID string `json:"_id"`
+	Date string `json:"date"`
+	City string `json:"city"`
+	State string `json:"state"`
+	Country string `json:"country"`
+	Shape string `json:"shape"`
+	Duration string `json:"duration"`
+	Description string `json:"description"`
+	ReportDate string `json:"report_date"`
+	HasImages bool `json:"has_images"`
+	Link string `json:"link"`
+	Latitude float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
