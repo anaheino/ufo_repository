1cf421e68cfda49b2a639206314c79cc90ff497b
Antti H <anaheino@gmail.com>
2023-10-17 19:25:46 +0300

    adding some scripts to help with aws

---
+++ ufo_backend/controllers/sightingController.go
@@ -0,0 +1,75 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"net/http"
+	"os"
+	"ufo_backend/structs"
+)
+
+var sightingDatabase *mongo.Database
+var defaultFindOptions *options.FindOptions
+
+func init() {
+	fmt.Printf("Initting")
+	if err:= godotenv.Load();err != nil {
+		log.Println("No .env file")
+	}
+	uri := os.Getenv("MONGODB_UFO_URI")
+	if uri == "" {
+		log.Fatal("Set the Mongodb uri for ufo database!")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	collation := options.Collation{
+		Locale:   "en",
+		Strength: 2, // 2 for case-insensitive, 1 for case-sensitive
+	}
+	findOptions := options.Find()
+	findOptions.SetCollation(&collation)
+	sightingDatabase = client.Database("i_want_to_believe")
+}
+
+func GetSightings(c *gin.Context) {
+	var sightingsCollection = sightingDatabase.Collection("sightings_with_coords")
+	var results []structs.Sighting
+
+	cursor, err := sightingsCollection.Find(context.TODO(), bson.D{{"country", "finland"}}, defaultFindOptions)
+	if err != nil {
+		fmt.Printf(err.Error())
+		fmt.Printf("No document was found with the title %s\n", "Finland")
+		return
+	}
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		fmt.Printf("No document was found with the country %s\n", "Finland")
+		return
+	}
+	fmt.Printf("%s", results[0].ReportDate)
+	c.IndentedJSON(http.StatusOK, results)
+}
+
+func GetByCountry(c *gin.Context) {
+	var results []structs.Sighting
+	var sightingsCollection = sightingDatabase.Collection("sightings_with_coords")
+	cursor, err := sightingsCollection.Find(context.TODO(), bson.D{{"country", "Finland"}})
+	if err != nil {
+		fmt.Printf(err.Error())
+		fmt.Printf("No document was found with the title %s\n", "Finland")
+		return
+	}
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		fmt.Printf("No document was found with the country %s\n", "Finland")
+		return
+	}
+	fmt.Printf("%s", results[0].ReportDate)
+	c.IndentedJSON(http.StatusOK, results)
+}
--- ufo_backend/main.go
+++ ufo_backend/main.go
@@ -1,59 +1,15 @@
 package main
 
-
 import (
-	"context"
-	"fmt"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"log"
-	"net/http"
-	"os"
-	"ufo_backend/structs"
+	"ufo_backend/controllers"
 )
 
-var sightingsCollection *mongo.Collection
-
-func getSightings(c *gin.Context) {
-	var results []structs.Sighting
-	cursor, err := sightingsCollection.Find(context.TODO(), bson.D{{"country", "Finland"}})
-	if err != nil {
-		fmt.Printf(err.Error())
-		fmt.Printf("No document was found with the title %s\n", "Finland")
-		return
-	}
-	if err = cursor.All(context.TODO(), &results); err != nil {
-		fmt.Printf("No document was found with the country %s\n", "Finland")
-		return
-	}
-	fmt.Printf("%s", results[0].ReportDate)
-	c.IndentedJSON(http.StatusOK, results)
-}
-
 func main() {
-	if err:= godotenv.Load();err != nil {
-		log.Println("No .env file")
-	}
-	uri := os.Getenv("MONGODB_UFO_URI")
-	if uri == "" {
-		log.Fatal("Set the Mongodb uri for ufo database!")
-	}
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		if err := client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
-	sightingsCollection = client.Database("i_want_to_believe").Collection("sightings_with_coords")
 	router := gin.Default()
 	router.Use(cors.Default())
-	router.GET("/sightings", getSightings)
+	router.GET("/sightings", controllers.GetSightings)
+	router.GET("/by-country", controllers.GetByCountry)
 	router.Run("localhost:8080")
 }
