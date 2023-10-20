704780aa572200d3a60317bd01a8748bca0399ee
Antti H <anaheino@gmail.com>
2023-10-20 15:17:41 +0300

    Added simple searching stub

---
+++ ufo_frontend/ufo-map/src/components/SearchBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card>
+    <v-toolbar
+        dense
+        floating
+    >
+      <v-text-field
+          v-model="searchTerm"
+          hide-details
+          single-line
+      ></v-text-field>
+      <v-btn @click="performSearch" icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+</template>
+
+
+<script>
+
+export default {
+  data() {
+    return {
+      searchTerm: '',
+    };
+  },
+  methods: {
+    performSearch() {
+      this.$emit('sighting-search-click', this.searchTerm);
+    },
+  },
+};
+</script>
--- ufo_frontend/ufo-map/src/views/HomeView.vue
+++ ufo_frontend/ufo-map/src/views/HomeView.vue
@@ -1,9 +1,30 @@
-<script setup lang="ts">
-import TheMap from '../components/TheMap.vue'
-</script>
-
 <template>
   <main>
+    <SearchBar @sighting-search-click="sightingSearch" />
     <TheMap />
   </main>
 </template>
+
+<script>
+import TheMap from '../components/TheMap.vue';
+import SearchBar from '../components/SearchBar.vue';
+
+export default {
+  components: {
+    SearchBar,
+    TheMap,
+  },
+  data() {
+    return {
+      searchData: null,
+    };
+  },
+  methods: {
+    sightingSearch(searchTerm) {
+      this.searchData = searchTerm;
+      console.log(this.searchData);
+    }
+  }
+};
+
+</script>
