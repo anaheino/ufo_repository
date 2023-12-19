8f4cabf3ef1c8c64c64bc176eff082d7de3ab73f
avh <anaheino@gmail.com>
2023-12-19 11:32:42 +0200

    Added container for vue3 application as well

--- ufo_frontend/ufo-map/vite.config.ts
+++ ufo_frontend/ufo-map/vite.config.ts
@@ -6,6 +6,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import vuetify from 'vite-plugin-vuetify';
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+    host: '0.0.0.0'
+  },
   plugins: [
     vue(),
     vueJsx(),
